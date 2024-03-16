b3278955cb2f1ec1c70d715ff2476c31bf2061a4
Ander <128938272+underDD@users.noreply.github.com>
2024-03-16 09:01:13 +0100

    Update head1.h

--- Clase1/head1.h
+++ Clase1/head1.h
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <time.h>
 
-#define L 128
+#define L 10
 #define V L*L
 #define N_DATA 10000
 #define N_Inter 100
