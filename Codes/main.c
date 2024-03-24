74a063317a4279466cfef3cebebb112aebdfaf1d
Ander <128938272+underDD@users.noreply.github.com>
2024-03-24 10:25:09 +0100

    Update main.c

--- Codes/main.c
+++ Codes/main.c
@@ -1,44 +1,87 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <time.h>
+#include "head1.h"
 
-#define L 16
-#define V L*L
-#define N_DATA 10000
-#define N_Inter 100
-#define fran rand()/((double)RAND_MAX+1)
-#define NormRAnu (2.3283063671E-10F)
+int main(){
 
-#define ACEPTANCIA
-#define TERMALIZACION
-//#define SIMULACION
+    srand(time(NULL));
 
-typedef struct Parameters {
-    int flag;
-    float b_0;
-    float b_f;
-    float dB;
-    int Nterm;
-    int Nmed;
-    int Nmc;
-} Parameters;
+    int S[V];
+    int stepsBeta, pasoBeta;
+    int pasoTerm;
+    int pasoMed;
+    int pasoMC;
+    int hyst;
+    int ele;
+    int xp[L],yp[L],xm[L],ym[L];
 
-int xp[L],yp[L],xm[L],ym[L];
-int Ntot, Nacep;
-// alogrithmsDATA.c : algoritmos para tratar los datos
+    FILE *fout;
 
-int loadParameters(Parameters *parameters);
-void saveconfig(int *config);
+    double beta;
+    double prob[5];
 
-// algorithmsNET.C : algoritmos para tratar la red
+    Parameters p;
 
-double parisirapuano(void);
-void genconfig(int *config, Parameters p);
-int spinrandom(void);
-void offsets(int *xp,int *yp,int *xm,int *ym);
-float energia(int *S);
-float magneto(int *S);
-void probabilidad(double *prob, double beta);
-void metropolis(int *s, double *prob);
+    char name[20];
+
+    loadParameters(&p);
+    printf("Beta inicial --> %f\n",p.b_0);
+    printf("Beta final --> %f\n",p.b_f);
+    printf("Paso beta --> %f\n",p.dB);
+    printf("Pasos Montecarlo -->  %d\n",p.Nmc);
+    printf("Numero medidas --> %d\n",p.Nmed);
+    printf("Pasos termalizacion --> %d\n",p.Nterm);
+
+    beta = p.b_0;
+    stepsBeta = (int)((p.b_f-p.b_0)/p.dB); // Calcula cuántos pasos hay que hacer para recorrer todo el paso de betas
+
+    genconfig(S,p);
+    offsets(xp,yp,xm,ym);
+
+    #ifdef TERMALIZACION
+
+        hyst = 0;
+        probabilidad(prob,beta);
+        sprintf(name,"results/term_%d_%d_%.2lf.txt",p.Nterm,L,beta);
+        fout = fopen(name,"wt");
+        for(pasoTerm=0;pasoTerm < p.Nterm;pasoTerm++)
+        {
+            metropolis(S,prob);
+            fprintf(fout,"%d\t%f\t%f\n",pasoTerm, energia(S), magneto(S));
+        }
+        fclose(fout);
+
+    #endif // TERMALIZACION
+
+    #ifdef SIMULACION
+
+    for(hyst=0;hyst<2;hyst++)
+        for(pasoBeta=0;pasoBeta<stepsBeta;pasoBeta++)
+        {
+            probabilidad(prob,beta);
+            sprintf(name,"results/med_%d_%d_%lf.txt",hyst,L,beta);
+            fout = fopen(name,"wt");
+            for(pasoTerm=0;pasoTerm<p.Nterm;pasoTerm++)
+            {
+                metropolis(S,prob);
+                //printf("Paso de termalizacion %d comletado\n",pasoTerm);
+            }
+
+            for(pasoMed=0;pasoMed<p.Nmed;pasoMed++)
+            {
+                for(pasoMC=0;pasoMC<p.Nmc;pasoMC++)
+                    metropolis(S,prob);
+
+                 fprintf(fout,"%d\t%lf\t%lf\n",pasoMed, energia(S), magneto(S));
+            }
+            fclose(fout);
+        }
+
+    #endif
+
+    #ifdef ACEPTANCIA
+
+        printf("Aceptancia = %lf", (double)Nacep/Ntot);
+
+    #endif // ACEPTANCIA
+    return 0;
+
+}
