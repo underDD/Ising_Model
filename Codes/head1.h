81701b0394bb8c6e89264f9edbee5df3bb48ba14
Ander <128938272+underDD@users.noreply.github.com>
2024-03-24 10:10:28 +0100

    Update head1.h

--- Codes/head1.h
+++ Codes/head1.h
@@ -4,30 +4,41 @@
 #include <math.h>
 #include <time.h>
 
-#define L 10
+#define L 16
 #define V L*L
 #define N_DATA 10000
 #define N_Inter 100
 #define fran rand()/((double)RAND_MAX+1)
 #define NormRAnu (2.3283063671E-10F)
 
+#define ACEPTANCIA
+#define TERMALIZACION
+//#define SIMULACION
+
 typedef struct Parameters {
     int flag;
-    double b_0;
-    double b_f;
-    double dB;
+    float b_0;
+    float b_f;
+    float dB;
     int Nterm;
     int Nmed;
     int Nmc;
 } Parameters;
 
+int xp[L],yp[L],xm[L],ym[L];
+int Ntot, Nacep;
+// alogrithmsDATA.c : algoritmos para tratar los datos
+
 int loadParameters(Parameters *parameters);
+void saveconfig(int *config);
+
+// algorithmsNET.C : algoritmos para tratar la red
+
+double parisirapuano(void);
 void genconfig(int *config, Parameters p);
 int spinrandom(void);
-double parisirapuano(void);
 void offsets(int *xp,int *yp,int *xm,int *ym);
-void calculos(float *estm,float *e,float *esq, float *msq,int *S,int *xp,int *yp);
-float energia(int *xp,int *yp,int *S);
+float energia(int *S);
 float magneto(int *S);
-void saveconfig(int *config);
-void Histograma(double *I,double *H,double *delta,double *mx,double *mn);
+void probabilidad(float *prob, float beta);
+void metropolis(int *s, float *prob);
