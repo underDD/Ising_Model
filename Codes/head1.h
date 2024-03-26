95509028c81a6243b13cb593be9b41170dd64599
Ander <128938272+underDD@users.noreply.github.com>
2024-03-26 09:45:01 +0100

    Update head1.h

--- Codes/head1.h
+++ Codes/head1.h
@@ -11,6 +11,8 @@
 #define fran rand()/((double)RAND_MAX+1)
 #define NormRAnu (2.3283063671E-10F)
 
+#define MAX_STR_LEN 256
+
 #define ACEPTANCIA
 #define TERMALIZACION
 //#define SIMULACION
@@ -25,20 +27,29 @@ typedef struct Parameters {
     int Nmc;
 } Parameters;
 
-int xp[L],yp[L],xm[L],ym[L];
-int Ntot, Nacep;
+extern int xp[L],yp[L],xm[L],ym[L];
+extern int Ntot, Nacep;
+
 // alogrithmsDATA.c : algoritmos para tratar los datos
 
-int loadParameters(Parameters *parameters);
-void saveconfig(int *config);
+extern int loadParameters(Parameters *parameters);
+extern void saveconfig(int *config);
+extern void calculos(double *e,double *m, int NDATA, double *results);
+extern double X(double *m, int NDATA);
+extern double Cv(double *e, int NDATA);
+extern void histograma(double *DATA,double *H, int NDATA, double *maxi, double *mini, double *delta);
+extern double e2(double e);
+extern double m2(double m);
+extern double mean(double *DATA,int NDATA);
+extern double Var(double *DATA, int NDATA);
 
 // algorithmsNET.C : algoritmos para tratar la red
 
-double parisirapuano(void);
-void genconfig(int *config, Parameters p);
-int spinrandom(void);
-void offsets(int *xp,int *yp,int *xm,int *ym);
-float energia(int *S);
-float magneto(int *S);
-void probabilidad(double *prob, double beta);
-void metropolis(int *s, double *prob);
+extern double parisirapuano(void);
+extern void genconfig(int *config, Parameters p);
+extern int spinrandom(void);
+extern void offsets();
+extern float energia(int *S);
+extern float magneto(int *S);
+extern void probabilidad(float *prob, float beta);
+extern void metropolis(int *s, float *prob);
