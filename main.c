ab84fb8f32575ead25308cb2b10528320ac75e8c
Ander Gabarrus <843213@unizar.es>
2024-04-02 13:53:23 +0200

    histereiss2

--- main.c
+++ main.c
@@ -1,92 +1,134 @@
 #include "head1.h"
 
-int main()
-{
 
-    Parameters p; 
-    loadParameters(&p);
+int main(){
 
-    float e[p.Nmed], m[p.Nmed];
-    float beta;
-    float em, mm, Cvm, Xm;
+    srand(time(NULL));
 
-    FILE *fin;
+    int S[V];
+    int stepsBeta, pasoBeta;
+    int pasoTerm;
+    int pasoMed;
+    int pasoMC;
+    int hyst;
+
     FILE *fout;
 
-    int i,j;
-    int hyst;
-    int nFich;
+    float beta;
+    float prob[5];
+    float e,m;
+
+    Parameters p;
+
+    clock_t time; // Esto es para calcular el tiempo de ejecucion
 
     char name[MAX_STR_LEN];
 
-    nFich = (int)((p.b_f-p.b_0)/p.dB);
+    loadParameters(&p);
+    printf("Beta inicial --> %f\n",p.b_0);
+    printf("Beta final --> %f\n",p.b_f);
+    printf("Paso beta --> %f\n",p.dB);
+    printf("Pasos Montecarlo -->  %d\n",p.Nmc);
+    printf("Numero medidas --> %d\n",p.Nmed);
+    printf("Pasos termalizacion --> %d\n",p.Nterm);
 
     beta = p.b_0;
-    hyst = 0;
-    j = 0;
-    fout = fopen("results/results.txt","wt");
+    stepsBeta = (int)((p.b_f-p.b_0)/p.dB); // Calcula cuántos pasos hay que hacer para recorrer todo el paso de betas
+    time = clock();
 
+    genconfig(S,p);
+    offsets();
 
-    for(i=0;i<nFich;i++)
-    {   sprintf(name,"results/med_%d_%d_%.2f.txt",hyst,L,beta);
-        fin = fopen(name,"rt");
-        em = mm = Cvm = Xm = 0;
+    #ifdef TERMALIZACION
 
-        if (fin == NULL) {printf("ERROR leyendo el fichero de beta %f",beta); exit(1);}
         
-        while(!feof(fin))
-        {
-            
-            fscanf(fin,"%d",&j);
-            fscanf(fin,"%f%f",&e[j],&m[j]);
+        probabilidad(prob,beta);
+        sprintf(name,"results/term_%d_%d_%.2f.txt",p.Nterm,L,beta);
+        fout = fopen(name,"wt");
 
+        e = m = 0;
+        e = energia(S);
+        m = fabs(magneto(S));
+        fprintf(fout,"%d\t%f\t%f\n",0, e, m);
+
+        for(pasoTerm=0;pasoTerm < p.Nterm;pasoTerm++)
+        {
+            e = m = 0;
+
+            metropolis(S,prob); 
+            e = energia(S);
+            m = fabs(magneto(S));
+            
+            fprintf(fout,"%d\t%f\t%f\n",pasoTerm, e, m);
         }
 
-        em = mean(e,p.Nmed);
-        mm = fabs(mean(m,p.Nmed));
-        Cvm = Cv(e,p.Nmed);
-        Xm = X(m,p.Nmed);
-        fprintf(fout, "%d,%f\t%f\t%f\t%f\t%f\n",hyst, beta, em, mm , Cvm, Xm);
+        saveconfig(S);
+        fclose(fout);
 
-        beta += p.dB;
-        fclose(fin);
 
-    }
-    hyst = 1;
-    j = 0;
-    beta = p.b_0;
-for(i=0;i<nFich+1;i++)
-    {   sprintf(name,"results/med_%d_%d_%.2f.txt",hyst,L,beta);
-        fin = fopen(name,"rt");
-        em = mm = Cvm = Xm = 0;
+    #endif // TERMALIZACION
 
-        if (fin == NULL) {printf("ERROR leyendo el fichero de beta %f",beta); exit(1);}
-        
-        while(!feof(fin))
+    #ifdef SIMULACION
+
+        hyst = 0;
+
+        for(pasoBeta=-stepsBeta;pasoBeta<stepsBeta+1;pasoBeta++)
         {
-            
-            fscanf(fin,"%d",&j);
-            fscanf(fin,"%f%f",&e[j],&m[j]);
 
+            probabilidad(prob,beta);
+            sprintf(name,"results/med_%d_%d_%.2f.txt",hyst,L,beta);
+            fout = fopen(name,"wt");
+
+            for(pasoTerm=0;pasoTerm < p.Nterm;pasoTerm++)
+            {
+                
+                e = m = 0;
+                metropolis(S,prob); 
+                e = energia(S);
+                m = fabs(magneto(S));
+
+            }
+
+            for(pasoMed=0;pasoMed<p.Nmed;pasoMed++)
+            {
+                e = m = 0;
+                for(pasoMC=0;pasoMC<p.Nmc;pasoMC++)
+                {
+                    metropolis(S,prob);
+                }
+                
+                e = energia(S);
+                m = magneto(S);
+
+                fprintf(fout,"%d\t%f\t%f\n",pasoMed, e, m);
+            }
+
+            beta+=p.dB;
+
+            if(pasoBeta == -1){
+            p.dB = -p.dB;
+            hyst = 1;
+            }
+
+
+            
+            fclose(fout);
         }
 
-        em = mean(e,p.Nmed);
-        mm = fabs(mean(m,p.Nmed));
-        Cvm = Cv(e,p.Nmed);
-        Xm = X(m,p.Nmed);
-        fprintf(fout, "%d\t%f\t%f\t%f\t%f\t%f\n",hyst, beta, em, mm , Cvm, Xm);
-
-        beta += p.dB;
-        fclose(fin);
-
-    }
+    #endif // SIMULACION
 
 
+    #ifdef ACEPTANCIA
 
+        printf("Aceptancia = %lf", (double)Nacep/Ntot);
 
+    #endif // ACEPTANCIA
 
+    time = clock() - time;
+    double time_taken = ((double)time)/CLOCKS_PER_SEC;
 
-    printf("FIN");
-    fclose(fout);
+    printf("\nTiempo de CPU (s) --> %lf\n ", time_taken);
+
     return 0;
+
 }
